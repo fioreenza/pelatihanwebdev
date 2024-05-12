a374c186bc728dd8ef19b967ce5f1fe3bef26348
fioreenza <fiorenzanalle05@gmail.com>
2024-05-12 22:13:24 +0700

    project setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,12 +8,19 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'],
       },
-    },
+      fontWeight: {
+        'light': '300',
+        'normal': '400',
+        'medium': '500',
+        'semibold': '600',
+        'bold': '700',
+        'extrabold': '800',
+        'black': '900',
+      },
+    }
   },
   plugins: [],
 };
